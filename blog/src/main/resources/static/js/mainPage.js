53a688ce82d1c78a0e3f47a21c49c5942ded5af6
Jeongmo.Seo <wjdah1402@gmail.com>
2024-02-28 16:23:29 +0900

    :lipstick: style: edit style of comment buttons and make a reply box with js function

--- blog/src/main/resources/static/js/mainPage.js
+++ blog/src/main/resources/static/js/mainPage.js
@@ -147,6 +147,85 @@ if (createCommentBtn) {
     })
 }
 
+// Show button related comment when mouseover
+const commentBodies = document.querySelectorAll('.article-view-comment-body');
+if (commentBodies) {
+    commentBodies.forEach(comment => {
+        let commentButtons = comment.querySelector('.article-view-comment-button-container');
+        comment.addEventListener('mouseover', () => {
+            commentButtons.style.display = 'inline-block';
+        })
+
+        comment.addEventListener('mouseout', () => {
+            commentButtons.style.display = 'none';
+        })
+    })
+}
+
+//Show delete and modify button when author is same as user
+const commentButtonContainer = document.querySelectorAll('.article-view-comment-button-container');
+if (commentButtonContainer) {
+    commentButtonContainer.forEach(button => {
+
+        let authorId = button.closest('.article-view-comment-body')
+            .querySelector('.article-view-comment-author')
+            .querySelector('input').value;
+
+        let userId = document.getElementById('user-id').value;
+        let modifyButton = button.querySelector('.article-view-modify-comment-btn');
+        let deleteButton = button.querySelector('.article-view-delete-comment-btn');
+
+        if (userId !== authorId) {
+            modifyButton.style.display = 'none';
+            deleteButton.style.display = 'none';
+        }
+    })
+}
+
+function replyElement(element, nickname) {
+    // article-view-reply-body
+    let replyBody = document.createElement('div');
+    replyBody.classList.add('article-view-reply-body');
+
+    // article-view-reply-author
+    let authorDiv = document.createElement('div');
+    authorDiv.classList.add('article-view-reply-author');
+    let authorInput = document.createElement('input');
+    authorInput.type = 'hidden';
+    authorInput.value = document.getElementById('user-id').value;
+    let authorParagraph = document.createElement('p');
+    authorParagraph.textContent = document.getElementById('user-nickname').value;
+    authorDiv.appendChild(authorInput);
+    authorDiv.appendChild(authorParagraph);
+
+    // article-view-reply-content
+    let contentDiv = document.createElement('div');
+    contentDiv.classList.add('article-view-reply-content');
+    let contentInput = document.createElement('input');
+    // 필요에 따라 contentInput 속성 설정
+    contentDiv.appendChild(contentInput);
+
+    // article-view-reply-button-container
+    let buttonContainerDiv = document.createElement('div');
+    buttonContainerDiv.classList.add('article-view-reply-button-container');
+    let cancelButton = document.createElement('button');
+    cancelButton.classList.add('article-view-cancel-reply-btn');
+    cancelButton.textContent = 'cancel';
+    let submitButton = document.createElement('button');
+    submitButton.classList.add('article-view-submit-reply-btn');
+    submitButton.textContent = 'submit';
+    buttonContainerDiv.appendChild(cancelButton);
+    buttonContainerDiv.appendChild(submitButton);
+
+    //append child
+    replyBody.appendChild(authorDiv);
+    replyBody.appendChild(contentDiv);
+    replyBody.appendChild(buttonContainerDiv);
+
+    //add element below element
+    element.insertAdjacentElement('afterend', replyBody);
+}
+
 // check nickname is same as author. return boolean
 function isSameAuthor(nickname) {
     let author = document.getElementById('article-view-author').textContent.replace("Writer: ", "");
