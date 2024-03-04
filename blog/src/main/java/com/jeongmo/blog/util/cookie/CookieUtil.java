35aa3cb5bc543a32b828778778c4a22a034973e5
Jeongmo.Seo <wjdah1402@gmail.com>
2024-03-04 11:45:23 +0900

    :sparkles: feat: add CookieUtil to make jwt token

---
+++ blog/src/main/java/com/jeongmo/blog/util/cookie/CookieUtil.java
@@ -0,0 +1,44 @@
+package com.jeongmo.blog.util.cookie;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.SerializationUtils;
+
+import java.util.Base64;
+
+public class CookieUtil {
+    public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+    public void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return;
+        }
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(name)) {
+                cookie.setValue("");
+                cookie.setPath("/");
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+    }
+
+    public String serializeCookie(Object obj) {
+        return Base64.getEncoder()
+                .encodeToString(SerializationUtils.serialize(obj));
+    }
+
+    public <T> T deserialize(Cookie cookie, Class<T> cls) {
+        return cls.cast(
+                SerializationUtils.deserialize(Base64.getDecoder().decode(cookie.getValue()))
+        );
+    }
+}
