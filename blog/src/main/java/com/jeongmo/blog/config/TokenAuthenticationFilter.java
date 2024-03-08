1d0c4dc87279042e89c064cd0e31199f22a73389
Jeongmo.Seo <wjdah1402@gmail.com>
2024-03-08 14:44:00 +0900

    :sparkles: feat: add token authentication filter to check access token

---
+++ blog/src/main/java/com/jeongmo/blog/config/TokenAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package com.jeongmo.blog.config;
+
+import com.jeongmo.blog.config.jwt.TokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private static final String HEADER_AUTHORIZATION = "Authorization";
+    private static final String TOKEN_PREFIX = "Bearer ";
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader(HEADER_AUTHORIZATION);
+
+        String token = getAccessToken(authorization);
+        if (tokenProvider.validToken(token)) {
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    public String getAccessToken(String authorization) {
+        if (authorization != null && authorization.startsWith(TOKEN_PREFIX)) {
+            return authorization.substring(TOKEN_PREFIX.length());
+        }
+        return null;
+    }
+}
