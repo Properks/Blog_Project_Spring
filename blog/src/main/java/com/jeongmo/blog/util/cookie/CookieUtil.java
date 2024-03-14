e11d7af11477f428f3a612f7e51d256b927139c3
Jeongmo.Seo <wjdah1402@gmail.com>
2024-03-14 14:36:29 +0900

    :zap: fix: add static to CookieUtil function to use it in other class without object

--- blog/src/main/java/com/jeongmo/blog/util/cookie/CookieUtil.java
+++ blog/src/main/java/com/jeongmo/blog/util/cookie/CookieUtil.java
@@ -8,14 +8,14 @@ import org.springframework.util.SerializationUtils;
 import java.util.Base64;
 
 public class CookieUtil {
-    public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setPath("/");
         cookie.setMaxAge(maxAge);
         response.addCookie(cookie);
     }
 
-    public void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
         Cookie[] cookies = request.getCookies();
         if (cookies == null) {
             return;
@@ -31,12 +31,12 @@ public class CookieUtil {
         }
     }
 
-    public String serializeCookie(Object obj) {
+    public static String serializeCookie(Object obj) {
         return Base64.getEncoder()
                 .encodeToString(SerializationUtils.serialize(obj));
     }
 
-    public <T> T deserialize(Cookie cookie, Class<T> cls) {
+    public static <T> T deserialize(Cookie cookie, Class<T> cls) {
         return cls.cast(
                 SerializationUtils.deserialize(Base64.getDecoder().decode(cookie.getValue()))
         );
