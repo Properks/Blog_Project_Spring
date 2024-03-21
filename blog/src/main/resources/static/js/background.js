fb521549b7bfe0e4f7bd82b42ccea9b1249aa2a5
Jeongmo.Seo <wjdah1402@gmail.com>
2024-03-21 14:55:15 +0900

    :sparkles: add http api request to use access token of OAuth2

--- blog/src/main/resources/static/js/background.js
+++ blog/src/main/resources/static/js/background.js
@@ -22,3 +22,101 @@ if (urlParam.has("token")) {
     let token = urlParam.get("token");
     localStorage.setItem("accessToken", token);
 }
+
+// function httpViewRequest(url) {
+//     let token = localStorage.getItem("accessToken");
+//     let header = new Headers();
+//     if (token != null) {
+//         header.append("Authorization", "Bearer " + token)
+//     }
+//     let request = new Request(url, {
+//         method: "GET",
+//         headers: header
+//     });
+//
+//     fetch(request)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.text();
+//         })
+//         .then(html => {
+//             // Create a temporary container element
+//             let tempContainer = document.createElement('div');
+//             tempContainer.innerHTML = html;
+//
+//             // Extract and execute JavaScript code
+//             let scripts = tempContainer.getElementsByTagName('script');
+//             for (let i = 0; i < scripts.length; i++) {
+//                 eval(scripts[i].innerHTML);
+//             }
+//
+//             // Append HTML content to the document body
+//             document.body.innerHTML = tempContainer.innerHTML;
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+// }
+
+function httpApiRequest(url, method, body) {
+    let token = localStorage.getItem("accessToken");
+    let header = new Headers();
+    if (token != null) {
+        header.append("Authorization", "Bearer " + token);
+    }
+    header.append("Content-type", "application/json");
+    let request = new Request(url, {
+        method: method,
+        headers: header,
+        body: body
+    });
+
+    fetch(request)
+        .then(response => {
+            const refreshToken = getCookie('refresh_token');
+            if (response.status === 401 && refreshToken) {
+                fetch ('/api/token', {
+                    method: 'POST',
+                    headers: {
+                        Authorization: "Bearer " + localStorage.getItem('access_token'),
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        refreshToken: getCookie('refresh_token')
+                    })
+                })
+                    .then(response => response.json())
+                    .then(response => {
+                        const token = response.accessToken
+                        localStorage.setItem('access_token', token)
+                        httpApiRequest(method, url, body);
+                    })
+            }
+            else if (!response.ok && !(response.status === 201)) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function getCookie(key) {
+    var result = null;
+    var cookie = document.cookie.split(';');
+    cookie.some(function (item) {
+        item = item.replace(' ', '');
+
+        var dic = item.split('=');
+
+        if (key === dic[0]) {
+            result = dic[1];
+            return true;
+        }
+    });
+
+    return result;
+}
