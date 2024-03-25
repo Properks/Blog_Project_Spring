c3a04d168dc2b64e92236b69a682bf41e13dc0c4
Jeongmo.Seo <wjdah1402@gmail.com>
2024-03-25 13:46:49 +0900

    :art: fix: fix http status to 201

--- blog/src/main/java/com/jeongmo/blog/controller/TokenApiController.java
+++ blog/src/main/java/com/jeongmo/blog/controller/TokenApiController.java
@@ -4,6 +4,7 @@ import com.jeongmo.blog.dto.token.AccessTokenRequest;
 import com.jeongmo.blog.dto.token.AccessTokenResponse;
 import com.jeongmo.blog.service.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,6 @@ public class TokenApiController {
     public ResponseEntity<AccessTokenResponse> createAccess(@RequestBody AccessTokenRequest request) {
         String accessToken = refreshTokenService.createNewAccessToken(request.getRefreshToken());
         AccessTokenResponse response = new AccessTokenResponse(accessToken);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
